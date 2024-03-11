9452574fb4f310f6e466de5b6e47908630b64209
mavrin88 <mavrin_88@mail.ru>
2024-03-11 17:27:59 +0300

    Добавление команды синхронизации пользователей

---
+++ app/Console/Commands/UpdateUserCommand.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class UpdateUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:user {--id=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        if ($this->option('id') == 'all') {
+            $partners = DB::connection('manager')
+                ->table('partners')
+                ->get();
+
+            foreach ($partners as $partner) {
+                $userData = [
+                    'first_name' => $partner->name,
+                    'last_name' => $partner->name,
+                    'email' => $partner->email,
+                    'password' => $partner->password,
+                    'account_id' => rand(1, 100),
+                    'partner_id' => $partner->id,
+                    'update_at' => now(),
+                ];
+
+                try {
+                    DB::connection('mysql')
+                        ->table('users')
+                        ->where('partner_id', '=', $partner->id)
+                        ->update($userData);
+                } catch (\Exception $e) {
+                    continue;
+                }
+            }
+        }
+    }
+}
