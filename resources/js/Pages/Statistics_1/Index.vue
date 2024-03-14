12bbe6a2c8692222cd427ad78154c4d955b24d99
Юрий Марченко <melvin.melvin.rulit@gmail.com>
2024-03-14 23:14:57 +0200

    не знаю почему наверно проеб - возвращаю вывод конверсий -30 дней с сегодняшнего дня

--- resources/js/Pages/Statistics_1/Index.vue
+++ resources/js/Pages/Statistics_1/Index.vue
@@ -88,7 +88,10 @@ export default {
   },
   layout: Layout,
   props: {
-    tableConversion: Array
+    tableConversion: {
+      type: Object,
+      required: true
+    }
   },
   data: function() {
     return {
